f62ec61c0f17d800ad86897685303851d6153196
nishant <nishantpacharne16@gmail.com>
2023-09-10 09:56:01 +0530

    added contract logic

--- src/app/modules/discord/discord.controller.ts
+++ src/app/modules/discord/discord.controller.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Client, GatewayIntentBits } from 'discord.js';
 import httpStatus from 'http-status';
@@ -5,21 +7,45 @@ import ApiError from '../../../errors/ApiError';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 
+import {
+  Signer,
+  Contract,
+  Provider,
+  Transaction,
+  Serializer,
+  utils,
+} from "koilib";
+
 const grantRole = catchAsync(async (req, res) => {
+
+  let nftContract: Contract;
+  let accounts;
+
+  async function getContract() {
+    if (nftContract) return nftContract;
+
+    const provider = new Provider(["https://harbinger-api.koinos.io"]);
+    // const userAddress = accounts[0].address;
+    const nftContractAddress = "1PZMHh8PBkhE8WGNTTkeCorZdJBqVkibev"; // Replace with your NFT contract address
+    nftContract = new Contract({
+        id: nftContractAddress,
+        provider
+    });
+
+    // get the abi of nftContract
+    await nftContract.fetchAbi();
+
+    nftContract.abi.methods.balance_of.entry_point = Number(nftContract.abi.methods.balance_of["entry-point"]);
+    nftContract.abi.methods.balance_of.read_only = Number(nftContract.abi.methods.balance_of["read-only"]);
+
+    return nftContract;
+  }
+
+
+  
   const { userId, roleId, walletAddr } = req.body;
   const discordServerId = process.env.discordServerId;
   const TOKEN = process.env.botToken;
-
-  // const client = new Client({
-  //   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
-  // });
-
-  // await client.login(botToken);
-  // const guild = await client.guilds.fetch(discordServerId as string);
-  // const member = await guild.members.fetch(data.userId);
-  // const roles = member?.roles?.cache
-  //   .filter(role => role.id !== discordServerId) // Exclude the @everyone role
-  //   .map(role => role.name);
 
   const client = new Client({
     intents: [
@@ -40,34 +66,44 @@ const grantRole = catchAsync(async (req, res) => {
 
   console.log({ member });
 
-  if (!member) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found in the guild');
+  const contract = await getContract();
+  console.log('get supply')
+  const suppply = await contract.functions.balance_of({owner: walletAddr});
+  
+  if (suppply.result.value >= 1){
+    if (!member) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'User not found in the guild');
+    }
+  
+    const role = guild?.roles.cache.get(roleId);
+  
+    if (!role) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Role not found in the guild');
+    }
+  
+    member.roles
+      .add(role)
+      .then(() => {
+        sendResponse<any>(res, {
+          statusCode: httpStatus.OK,
+          success: true,
+          message: `Role added successfullydddddddd`,
+          // data: roles,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        sendResponse<any>(res, {
+          statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+          success: false,
+          message: error.message,
+        });
+      });
+  } else {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Insufficient balance');
   }
 
-  const role = guild?.roles.cache.get(roleId);
-
-  if (!role) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Role not found in the guild');
-  }
-
-  member.roles
-    .add(role)
-    .then(() => {
-      sendResponse<any>(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Role added successfully',
-        // data: roles,
-      });
-    })
-    .catch(error => {
-      console.error(error);
-      sendResponse<any>(res, {
-        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-        success: false,
-        message: error.message,
-      });
-    });
+  
 
   // if (roles.length < 1) {
   //   const role = await guild.roles.fetch(roleId as string);
